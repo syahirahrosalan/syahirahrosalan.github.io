8e0b80151a5a8834908101c4d6d4cd968665cab0
airaalan <syairah.alan@gmail.com>
2018-03-07 17:16:54 +0800

    remove unnecessary section

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "sagarshirbhate");
+    GitHubCalendar("#github-graph", "airaalan");
 
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({username: "sagarshirbhate", selector: "#ghfeed", limit: 20});
+    GitHubActivity.feed({username: "airaalan", selector: "#ghfeed", limit: 20});
 
 
 });
