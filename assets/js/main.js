df8f63a314e997e913f0a77231d6c0c6c84c6eb1
syahirahrosalan <34876986+syahirahrosalan@users.noreply.github.com>
2018-03-07 13:04:29 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "airaalan");
+    GitHubCalendar("#github-graph","rvitaliy");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
